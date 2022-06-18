ca1262706ba8631ebfafa4ef08e6e7f1af23d7a7
Alexander-Mini <alexander.molc@gmail.com>
2022-06-18 18:34:38 -0500

    Prueba

--- Gato.vhdl
+++ Gato.vhdl
@@ -13,8 +13,37 @@ end entity Gato;
 
 architecture sim of Gato is
 
+    signal clk_freq    : integer := 6e6;
+    signal counter     : integer;
+
+    signal row_counter : unsigned(2 downto 0);
+
 begin
-    filas(3) <= '1';
-    columnas(3) <= '1'; 
+    
+    COUNTER_PROC : process(clk)
+    begin
+        if rising_edge(clk) then
+            if counter = clk_freq - 1 then
+                counter <= 0;
+
+                row_counter <= row_counter + 1;
+
+            else
+                counter <= counter + 1; 
+
+
+
+            end if;
+        end if;
+    end process;
+
+    OUTPUT_PROC : process(row_counter)
+    begin
+        columnas(3) <= '1';
+
+        filas <= (others => '0');
+        filas(to_integer(row_counter)) <= '1';
+        
+    end process;
 
 end architecture sim;
